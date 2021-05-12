5b98ad49a65fb5eb2aa8257018f9b03be311a2c9
Ryan Cornel <ry.cornel@students.makeschool.com>
2021-05-11 23:30:58 -0700

    Commit 14
    Completed delete function.

--- app.py
+++ app.py
@@ -144,8 +144,12 @@ def delete(plant_id):
     # TODO: Make a `delete_one` database call to delete the plant with the given
     # id.
 
+    mongo.db.plants.delete_one({'_id' : ObjectId(plant_id)})
+
     # TODO: Also, make a `delete_many` database call to delete all harvests with
     # the given plant id.
+
+    mongo.db.plants.delete_many({'plant_id' : plant_id})
 
     return redirect(url_for('plants_list'))
 
